f1773cc4054d26e826e51ac7fd2508a62a79be67
tkuriyama <taro.kuriyama@gmail.com>
2020-10-19 21:30:47 -0400

    Server basic loop

--- courseserver/course_server.c
+++ courseserver/course_server.c
@@ -7,12 +7,34 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <arpa/inet.h>
+#include "coursedb.c"
+#include "coursedb.h"
 
 #define BUFSIZE 1024
 
 void error(char *msg) {
     perror(msg);
     exit(0);
+}
+
+void print_client_info(int newfd, struct sockaddr_storage *client_addr,
+		       socklen_t *addr_size) {
+  char ipstr[INET6_ADDRSTRLEN];
+  int port;
+  
+  if (getpeername(newfd, (struct sockaddr *)client_addr, addr_size) < 0) {
+    error("ERROR getpeername failed");
+  }
+  else {
+    struct sockaddr_in *s = (struct sockaddr_in *)client_addr;
+    port = ntohs(s->sin_port);
+    inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+    printf("> Client connected from: %s:%d\n", ipstr, port);
+  }
+}
+
+char *process_request(char *buf) {
+  return buf;
 }
 
 int main() {
@@ -23,51 +45,53 @@ int main() {
   int sockfd, newfd;
 
   char buf[BUFSIZE];
+  char response[BUFSIZE];
+
+  char *ip = "127.0.0.1";
+  char *port = "3490";
   
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
   hints.ai_socktype = SOCK_STREAM;
-  // hints.ai_flags = AI_PASSIVE;  
-  getaddrinfo("127.0.0.1", "3490", &hints, &res);
+  getaddrinfo(ip, port, &hints, &res);
 
   sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
   if (sockfd < 0) 
     error("ERROR opening socket");  
   if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0)
     error("ERROR binding socket");
-  printf("Started server on 127.0.0.1:3490\n");
-  
+  printf("> Started server on %s:%s\n", ip, port);
+    
   if (listen(sockfd, 20) < 0)
     error("ERROR listen failed");
   addr_size = sizeof client_addr;
-  newfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
 
-  // print client info
-  char ipstr[INET6_ADDRSTRLEN];
-  int port;
+  newfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+  print_client_info(newfd, &client_addr, &addr_size);
   
-  if (getpeername(newfd, (struct sockaddr *)&client_addr, &addr_size) < 0) {
-    error("ERROR getpeername failed");
-  }
-  else {
-    struct sockaddr_in *s = (struct sockaddr_in *)&client_addr;
-    port = ntohs(s->sin_port);
-    inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-    printf("Client IP address: %s\n", ipstr);
-    printf("Client port      : %d\n", port);    
-  }
-  
-  if (recv(newfd, &buf, BUFSIZE, 0) > 0) {
-    if (send(newfd, &buf, BUFSIZE, 0) > 0) 
-      printf("Echoed message: %s\n", buf);
-  }
-  else {
-    printf("Recv returned 0, client closed connection.\n");
+  while (1) {
+    if (recv(newfd, &buf, BUFSIZE, 0) > 0) {
+      if (strcmp(buf, "bye") == 0) {
+	printf("> Client asked to close connection.");
+      }
+      else {
+	printf("Received: %s", buf);
+	strcpy(response, process_request(buf));
+      }
+      if (send(newfd, &response, BUFSIZE, 0) > 0) 
+	printf("Sent: %s\n", response);
+      else
+	error("> ERROR failed to send");
+    }
+    else {
+      printf("> Recv returned 0, client closed connection.\n");
+      break;
+    }
   }
   
   close(sockfd);
   close(newfd);
   freeaddrinfo(res);
-  printf("Bye.\n");
+  printf("> Bye.\n");
 }
 

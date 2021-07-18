e11b7ec08ad0dea89e01264daacbdd066ddfb9bc
tkuriyama <taro.kuriyama@gmail.com>
2021-07-18 15:07:46 -0400

    Add naive loop

--- echoserver_python/echoclient.py
+++ echoserver_python/echoclient.py
@@ -1,22 +1,34 @@
 
 import asyncio # type: ignore
+import time # type: ignore
 
 
-async def tcp_echo_client(message):
+################################################################################
+
+
+async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
 
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
-    await writer.drain()
+    msg  = ''
+    while msg.lower() != 'bye' and writer:
+        msg = input('Enter message: ').strip()
+        print(f'Send: {msg}')
+        writer.write(msg.encode())
+        await writer.drain()
 
-    data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
+        data = await reader.read(100)
+        print(f'Received: {data.decode()!r}')
+
+        time.sleep(0.0001)
 
     print('Close the connection')
     writer.close()
     await writer.wait_closed()
 
 
+################################################################################
+
+
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client('Hello World!'))
+    asyncio.run(tcp_echo_client())
--- echoserver_python/echoserver.py
+++ echoserver_python/echoserver.py
@@ -1,19 +1,26 @@
 import asyncio # type: ignore
 
 
+################################################################################
+
+
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
 
-    print(f"Received {message!r} from {addr!r}")
+    while True:
+        data = await reader.read(100)
+        message = data.decode()
+        addr = writer.get_extra_info('peername')
 
-    print(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
+        print(f"Received {message!r} from {addr!r}")
 
-    print("Close the connection")
-    writer.close()
+        print(f"Send: {message!r}")
+        writer.write(data)
+        await writer.drain()
+
+        if message.lower().strip() == 'bye':
+            print("Close the connection")
+            writer.close()
+            break
 
 
 async def main():
@@ -25,6 +32,9 @@ async def main():
 
     async with server:
         await server.serve_forever()
+
+
+################################################################################
 
 
 if __name__ == '__main__':

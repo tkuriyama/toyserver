ecc5f47f942fca18d39be3176516825c1cc40361
tkuriyama <taro.kuriyama@gmail.com>
2021-07-18 14:22:30 -0400

    Adding Python example from the docs

---
+++ echoserver_python/echoclient.py
@@ -0,0 +1,22 @@
+
+import asyncio # type: ignore
+
+
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(f'Send: {message!r}')
+    writer.write(message.encode())
+    await writer.drain()
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+    print('Close the connection')
+    writer.close()
+    await writer.wait_closed()
+
+
+if __name__ == '__main__':
+    asyncio.run(tcp_echo_client('Hello World!'))
